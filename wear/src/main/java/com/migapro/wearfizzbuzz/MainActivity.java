cd148da094336927a037aa133f614d64846c43ca
miga <miguel.g.kano@gmail.com>
2015-02-28 21:44:14 -0600

    Position the views in layout

--- wear/src/main/java/com/migapro/wearfizzbuzz/MainActivity.java
+++ wear/src/main/java/com/migapro/wearfizzbuzz/MainActivity.java
@@ -2,7 +2,6 @@ package com.migapro.wearfizzbuzz;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -13,12 +12,5 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
     }
 }
